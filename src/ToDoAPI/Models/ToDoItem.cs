515a9cefb6958bb077738f40fec6b87933cf7e3d
balaji <balajisivakumaran@yahoo.co.in>
2023-08-14 21:28:55 +1200

    Introduce Db Contetx and APIS

--- src/ToDoAPI/Models/ToDoItem.cs
+++ src/ToDoAPI/Models/ToDoItem.cs
@@ -2,7 +2,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace ToDoAPI.Models
 {
-    public class ToDoItems
+    public class ToDoItem
     {
         [Key]
         public int Id { get; set; }
--- src/ToDoAPI/Program.cs
+++ src/ToDoAPI/Program.cs
@@ -1,9 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using ToDoAPI.Data;
+using ToDoAPI.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("ToDODb"));
 
 var app = builder.Build();
 
@@ -16,29 +22,25 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+app.MapGet("api/v1/todo", async(AppDbContext context) => {
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+   var toDoItems = await context.ToDoItems.ToListAsync();
+
+   return Results.Ok(toDoItems); 
+});
+
+app.MapPost("api/v1/todo", async(AppDbContext context, ToDoItem toDoItem) => {
+
+    if(toDoItem == null) return Results.BadRequest();
+
+    await context.ToDoItems.AddAsync(toDoItem);
+
+    await context.SaveChangesAsync();
+
+    return Results.Created($"api/v1/todo/{toDoItem.Id}", toDoItem);
+
+});
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+
